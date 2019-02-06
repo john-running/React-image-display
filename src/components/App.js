e9624bcfd40abc36d7c627deee38d57037a09aa2
John Running <john.a.running@gmail.com>
2019-02-06 12:49:43 -0500

    added friendly message for no results

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,23 @@ import SearchBar from './SearchBar'
 import ImageList from './ImageList'
 
 class App extends React.Component {
-state = {images: []};
+state = {images: [], term: ''};
 
   onSearchSubmit = async term => {
     const response = await unsplash.get('search/photos', {
-      params: { query: term },
+      params: { query: term,
+                per_page: 100
+      },
     });
     //console.log(response.data.results)
-    this.setState({ images: response.data.results })
+    this.setState({ images: response.data.results, term: term })
   }
 
   render(){
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
-        <ImageList images = {this.state.images}/>
+        <ImageList images = {this.state.images} term = {this.state.term}/>
       </div>
     );
   }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,14 +3,28 @@ import React from 'react';
 import ImageCard from './ImageCard';
 
 
-const ImageList = ({ images }) => {
-  const theimages = images.map(image => {
-    return (
-      <ImageCard key={image.id} image={image}/>
-    )
-  });
-  return <div className="image-list">{theimages}</div>;
+const ImageList = ({ images, term }) => {
+  let theimages = null
+  let responseHeaderMessage = null
 
+  if (images.length > 0) {
+    responseHeaderMessage = `Results for "${term}"`
+    theimages = images.map(image => {
+      return (
+        <ImageCard key={image.id} image={image}/>
+      )
+    });
+  } else if (term) {
+    responseHeaderMessage = `Sorry no results for "${term}"`
+  }
+
+  return (
+    <div>
+    <h2>{responseHeaderMessage}</h2>
+    <br />
+    <div className="image-list">{theimages}</div>
+    </div>
+  );
 };
 
 export default ImageList
